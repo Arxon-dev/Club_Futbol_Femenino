d6f5cdfe0179ae5f23e7b03561a23cff4d40d0bf
Arxon-dev <carlos.ortigosa@outlook.es>
2026-02-27 19:53:53 +0100

    fix: correct sequelize import in new models (default export, not destructured)

--- api/src/models/Match.js
+++ api/src/models/Match.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const sequelize = require('../config/database');
 
 const Match = sequelize.define('Match', {
   id: {
